e5c560b65565da117fa726e5e607b504fa8e88a4
shreyamunshi <shreyamm.cd.mat17@itbhu.ac.in>
2020-07-24 03:33:41 +0530

    Updates

---
+++ precache-manifest.1ff736f6b9269b888decdf43362627f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd769f0665fb1f74fc78b0cddb671353",
+    "url": "/SRB-MarsColonizationProgram/index.html"
+  },
+  {
+    "revision": "818121effd0a36d6908a",
+    "url": "/SRB-MarsColonizationProgram/static/css/main.8fc251e4.chunk.css"
+  },
+  {
+    "revision": "26a645a29a20184bb286",
+    "url": "/SRB-MarsColonizationProgram/static/js/2.289d3f89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SRB-MarsColonizationProgram/static/js/2.289d3f89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "818121effd0a36d6908a",
+    "url": "/SRB-MarsColonizationProgram/static/js/main.0c651913.chunk.js"
+  },
+  {
+    "revision": "5b5700d5c034964a1220",
+    "url": "/SRB-MarsColonizationProgram/static/js/runtime-main.261eff22.js"
+  }
+]);
